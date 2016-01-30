86458d43ea79341eadb371ac902d4ff3a0246ac6
Jamin <jamin@bitlag.net>
2016-01-29 17:40:12 -0800

    install.sh:  skip dot-git

--- install.sh
+++ install.sh
@@ -5,5 +5,8 @@ for dotfile in .*; do
   if [ x"$dotfile" = x"." ]; then continue; fi 
   if [ x"$dotfile" = x".." ]; then continue; fi 
 
+  # skip ".git"
+  if [ x"$dotfile" = x".git" ]; then continue; fi
+
   ln -vfs "$dotfiles"/"$dotfile" ~/
 done
